f31474b53c5e50e251fbc18c00898ed3cffcdea2
oruponu <oruponu@outlook.com>
2026-01-09 23:03:45 +0900

    別の駒がある場所への駒の配置を禁止

--- piece.gd
+++ piece.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 
 var is_held = false
+var current_col = -1
+var current_row = -1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,10 +24,20 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 			var row = floor(position.y / GameConfig.GRID_SIZE)
 			
 			if col >= 0 and col < GameConfig.BOARD_COLS and row >= 0 and row < GameConfig.BOARD_ROWS:
-				# 駒をマスの中央に配置
-				var new_x = (col * GameConfig.GRID_SIZE) + (GameConfig.GRID_SIZE / 2.0)
-				var new_y = (row * GameConfig.GRID_SIZE) + (GameConfig.GRID_SIZE / 2.0)
-				position = Vector2(new_x, new_y)
+				if GameManager.is_cell_empty(col, row) or (col == current_col and row == current_row):
+					GameManager.update_board_state(current_col, current_row, col, row, self)
+					current_col = col
+					current_row = row
+					
+					# 駒をマスの中央に配置
+					var new_x = (col * GameConfig.GRID_SIZE) + (GameConfig.GRID_SIZE / 2.0)
+					var new_y = (row * GameConfig.GRID_SIZE) + (GameConfig.GRID_SIZE / 2.0)
+					position = Vector2(new_x, new_y)
+				else:
+					# 別の駒があるため置けない
+					is_held = true
+					z_index = 10
 			else:
+				# 盤外のため置けない
 				is_held = true
 				z_index = 10
