1f1d0c5a3e848600a31f93ae1d3d3605d643cc24
oruponu <oruponu@outlook.com>
2026-01-12 17:41:07 +0900

    1手読みのAIを実装

--- ai_player.gd
+++ ai_player.gd
@@ -3,6 +3,29 @@ class_name AIPlayer
 extends RefCounted
 
 
+const PIECE_VALUES = {
+	Piece.Type.PAWN: 1,
+	Piece.Type.LANCE: 3,
+	Piece.Type.KNIGHT: 4,
+	Piece.Type.SILVER: 5,
+	Piece.Type.GOLD: 6,
+	Piece.Type.BISHOP: 8,
+	Piece.Type.ROOK: 10,
+	Piece.Type.KING: 99
+}
+
+const PROMOTION_BONUS_VALUES = {
+	Piece.Type.PAWN: 5,
+	Piece.Type.LANCE: 3,
+	Piece.Type.KNIGHT: 2,
+	Piece.Type.SILVER: 1,
+	Piece.Type.GOLD: 0,
+	Piece.Type.BISHOP: 3,
+	Piece.Type.ROOK: 3,
+	Piece.Type.KING: 0
+}
+
+
 var is_enemy_side: bool = true
 
 
@@ -11,13 +34,36 @@ func get_next_move(main_node: Node2D) -> Move:
 	
 	if legal_moves.is_empty():
 		return null
+		
+	var best_score = 0
+	var best_moves: Array[Move] = []
 	
-	var chosen_move = legal_moves.pick_random()
+	for move in legal_moves:
+		if _can_promote(move):
+			move.is_promotion = true
+		
+		var score = _evaluate_move_score(move, main_node)
+		
+		if score > best_score:
+			best_score = score
+			best_moves = [move]
+		elif score == best_score:
+			best_moves.append(move)
 	
-	if _can_promote(chosen_move):
-		chosen_move.is_promotion = (randi() % 2 == 0)
+	return best_moves.pick_random()
+
+
+func _evaluate_move_score(move: Move, main_node: Node2D) -> int:
+	var score = 0
 	
-	return chosen_move
+	var target_piece = main_node.get_piece(move.to_col, move.to_row)
+	if target_piece != null:
+		score += PIECE_VALUES.get(target_piece.piece_type, 0)
+	
+	if move.is_promotion:
+		score += PROMOTION_BONUS_VALUES.get(move.piece.piece_type, 0)
+	
+	return score
 
 
 func _get_all_legal_moves(main_node: Node2D) -> Array[Move]:
