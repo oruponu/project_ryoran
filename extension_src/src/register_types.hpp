dfba5c4be4b67a01b8902f2414b8d148a70e795e
oruponu <oruponu@outlook.com>
2026-01-13 23:31:45 +0900

    GDExtensionの登録処理を実装

---
+++ extension_src/src/register_types.hpp
@@ -0,0 +1,11 @@
+#ifndef REGISTER_TYPES_HPP
+#define REGISTER_TYPES_HPP
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_shogi_ai_module(ModuleInitializationLevel p_level);
+void uninitialize_shogi_ai_module(ModuleInitializationLevel p_level);
+
+#endif
