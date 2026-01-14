c6a71bc91ac9721ea0bab650155f7501a39c679d
oruponu <oruponu@outlook.com>
2026-01-14 12:43:55 +0900

    移動データの構造体を作成

--- extension_src/src/shogi_utils.hpp
+++ extension_src/src/shogi_utils.hpp
@@ -15,6 +15,25 @@ const int BOARD_SIZE = 81;
 // 駒の種類数
 const int PIECE_TYPE_COUNT = 8;
 
+struct Move {
+    uint8_t from_col;
+    uint8_t from_row;
+    uint8_t to_col;
+    uint8_t to_row;
+    uint8_t piece_type;
+    bool is_promotion;
+    bool is_drop;
+    bool is_capture;
+
+    Move()
+        : from_col(0), from_row(0), to_col(0), to_row(0), piece_type(EMPTY), is_promotion(false), is_drop(false),
+          is_capture(false) {}
+
+    Move(int fc, int fr, int tc, int tr, int pt, bool promo, bool drop, bool capture)
+        : from_col((uint8_t)fc), from_row((uint8_t)fr), to_col((uint8_t)tc), to_row((uint8_t)tr),
+          piece_type((uint8_t)pt), is_promotion(promo), is_drop(drop), is_capture(capture) {}
+};
+
 } // namespace Shogi
 
 #endif
