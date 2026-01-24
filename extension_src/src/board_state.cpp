4b49638ca4f03ed0a797a51ed36b9cc875f3e7c9
oruponu <oruponu@outlook.com>
2026-01-25 01:13:53 +0900

    走り駒の利きのBitboardを実装

--- extension_src/src/board_state.cpp
+++ extension_src/src/board_state.cpp
@@ -243,7 +243,106 @@ void BoardState::initialize_attack_tables() {
         }
     }
 
+    // 走り駒
+    int dxs[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+    int dys[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    for (int index = 0; index < Shogi::BOARD_SIZE; ++index) {
+        int col = index / Shogi::BOARD_ROWS;
+        int row = index % Shogi::BOARD_ROWS;
+
+        for (int dir = 0; dir < 8; ++dir) {
+            Bitboard ray;
+            Coord coord = {col + dxs[dir], row + dys[dir]};
+            while (coord.is_valid()) {
+                ray.set(coord.col * Shogi::BOARD_ROWS + coord.row);
+                coord.col += dxs[dir];
+                coord.row += dys[dir];
+            }
+            rays_[dir][index] = ray;
+        }
+    }
+
     attack_tables_initialized_ = true;
+}
+
+Bitboard BoardState::get_lance_attacks(int square, Turn turn, const Bitboard &occupancy) const {
+    int direction = (turn == Turn::SENTE) ? 0 : 4; // 0: 上，4: 下
+    Bitboard ray = rays_[direction][square];
+    Bitboard blockers = ray & occupancy;
+
+    if (!blockers.is_empty()) {
+        int blocker_index;
+        if (turn == Turn::SENTE) {
+            blocker_index = blockers.msb();
+        } else {
+            blocker_index = blockers.lsb();
+        }
+
+        Bitboard ray_from_blocker = rays_[direction][blocker_index];
+        return ray & ~ray_from_blocker;
+    }
+
+    return ray;
+}
+
+Bitboard BoardState::get_bishop_attacks(int square, const Bitboard &occupancy) const {
+    Bitboard attacks;
+    int directions[4] = {1, 3, 5, 7}; // 1: 右上，3: 右下，5: 左下，7: 左上
+
+    for (int dir = 0; dir < 4; ++dir) {
+        int direction = directions[dir];
+        Bitboard ray = rays_[direction][square];
+        Bitboard blockers = ray & occupancy;
+
+        if (!blockers.is_empty()) {
+            int blocker_index;
+
+            // 左下と左上はindexが減る方向
+            // 右上と右下はindexが増える方向
+            if (direction == 5 || direction == 7) {
+                blocker_index = blockers.msb();
+            } else {
+                blocker_index = blockers.lsb();
+            }
+
+            Bitboard ray_from_blocker = rays_[direction][blocker_index];
+            attacks |= (ray & ~ray_from_blocker);
+        } else {
+            attacks |= ray;
+        }
+    }
+
+    return attacks;
+}
+
+Bitboard BoardState::get_rook_attacks(int square, const Bitboard &occupancy) const {
+    Bitboard attacks;
+    int directions[4] = {0, 2, 4, 6}; // 0: 上，2: 右，4: 下，6: 左
+
+    for (int dir = 0; dir < 4; ++dir) {
+        int direction = directions[dir];
+        Bitboard ray = rays_[direction][square];
+        Bitboard blockers = ray & occupancy;
+
+        if (!blockers.is_empty()) {
+            int blocker_index;
+
+            // 上と左はindexが減る方向
+            // 下と右はindexが増える方向
+            if (direction == 0 || direction == 6) {
+                blocker_index = blockers.msb();
+            } else {
+                blocker_index = blockers.lsb();
+            }
+
+            Bitboard ray_from_blocker = rays_[direction][blocker_index];
+            attacks |= (ray & ~ray_from_blocker);
+        } else {
+            attacks |= ray;
+        }
+    }
+
+    return attacks;
 }
 
 void BoardState::build_bitboard() {
--- extension_src/src/board_state.hpp
+++ extension_src/src/board_state.hpp
@@ -41,6 +41,10 @@ class BoardState {
     inline static Bitboard attacks_gold_[2][Shogi::BOARD_SIZE];
     inline static Bitboard attacks_king_[Shogi::BOARD_SIZE];
 
+    // 走り駒の利き
+    inline static Bitboard sttacks_lance_[2][Shogi::BOARD_SIZE];
+    inline static Bitboard rays_[8][Shogi::BOARD_SIZE];
+
     // すべての駒のBitboard
     Bitboard bitboard_all_;
 
@@ -64,6 +68,8 @@ class BoardState {
 
     static void initialize_attack_tables();
 
+    static Bitboard get_ray(int square, int direction) { return rays_[direction][square]; }
+
     static const Bitboard &get_pawn_attacks(int square, Shogi::Turn turn) {
         return attacks_pawn_[static_cast<int>(turn)][square];
     }
@@ -81,6 +87,20 @@ class BoardState {
     }
 
     static const Bitboard &get_king_attacks(int square) { return attacks_king_[square]; }
+
+    Bitboard get_lance_attacks(int square, Shogi::Turn turn, const Bitboard &occupancy) const;
+
+    Bitboard get_bishop_attacks(int square, const Bitboard &occupancy) const;
+
+    Bitboard get_rook_attacks(int square, const Bitboard &occupancy) const;
+
+    Bitboard get_promoted_bishop_attacks(int square, const Bitboard &occupancy) const {
+        return get_bishop_attacks(square, occupancy) | get_king_attacks(square);
+    }
+
+    Bitboard get_promoted_rook_attacks(int square, const Bitboard &occupancy) const {
+        return get_rook_attacks(square, occupancy) | get_king_attacks(square);
+    }
 
     void build_bitboard();
     void add_piece_to_bitboard(int index, Shogi::Turn turn, Shogi::PieceType type, bool is_promoted);
